7f351ce1cd33a941352c3564f905ca06393820e4
NeoArmageddon <neoarmageddon@web.de>
2014-01-23 23:11:42 +0100

    Missionname on loadingscreen now shorter.

--- include/defines.hpp
+++ include/defines.hpp
@@ -1,2 +1,3 @@
-#define VERSION "1.7.1"
-#define MISSIONNAME "co10 Escape Altis 1.7.1"
+#define VERSION "1.7 Dev3"
+#define MISSIONNAME "Escape Altis"
+#define MISSIONNAMEFULL "co10 Escape Altis 1.7 Dev3"
